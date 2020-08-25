4c744f94e9ec9f8ed5ceb30a676c47d9f6db82e7
Jonathan Durand <jonathan.durand@anteagroup.com>
2020-08-25 10:36:52 +0200

    se production env in wsgi.py

--- onadata/apps/main/wsgi.py
+++ onadata/apps/main/wsgi.py
@@ -1,6 +1,6 @@
 from django.core.wsgi import get_wsgi_application
 
 import os
-os.environ["DJANGO_SETTINGS_MODULE"] = "onadata.settings.kc_environ"
+os.environ["DJANGO_SETTINGS_MODULE"] = "onadata.settings.production"
 
 application = get_wsgi_application()
