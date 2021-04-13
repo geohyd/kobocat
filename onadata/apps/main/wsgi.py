26712076ac37c4a77e66c14277df702e26abc32a
JD2701 <jonathan.durand@anteagroup.com>
2021-04-13 20:50:32 +0200

    remove unsed code

--- onadata/apps/main/wsgi.py
+++ onadata/apps/main/wsgi.py
@@ -3,7 +3,5 @@ from __future__ import unicode_literals, print_function, division, absolute_impo
 
 from django.core.wsgi import get_wsgi_application
 
-import os
-os.environ["DJANGO_SETTINGS_MODULE"] = "onadata.settings.production"
 
 application = get_wsgi_application()
